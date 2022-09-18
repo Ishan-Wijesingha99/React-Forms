d2ecc7e7f6dde996fd3d1dadb7f1f34868fc74df
Ishan <ishanwijes@gmail.com>
2022-09-19 00:48:56 +1000

    radio buttons completed

--- src/components/FormsStateObject.js
+++ src/components/FormsStateObject.js
@@ -6,7 +6,14 @@ import React from "react";
 // using form state object for forms with many input fields
 export const FormWithManyFields = function() {
     // make the thing that is changing state an object, not a single variable, that way you can have multiple properties of the object as a state change
-    const [inputObject, setInputObject] = React.useState({firstName: '', lastName: '', email: ''})
+    const [inputObject, setInputObject] = React.useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        comments: '',
+        isFriendly: false,
+        employment: ''
+    })
 
     console.log(inputObject)
 
@@ -15,7 +22,7 @@ export const FormWithManyFields = function() {
         setInputObject(prevObject => {
             return {
                 ...prevObject,
-                [event.target.name]: event.target.value
+                [event.target.name]: event.target.type === 'checkbox' ? event.target.checked : event.target.value
             }
         })
 
@@ -44,6 +51,69 @@ export const FormWithManyFields = function() {
                 name="email"
                 value={inputObject.email}
             />
+
+
+            {/* textarea is not self-closing in HTML like an input tag, but in React it is */}
+            <textarea 
+                value={inputObject.comments}
+                placeholder="Comments"
+                onChange={handleInputChange}
+                name="comments"
+            />
+
+
+            {/* checkboxes are different because they only store booleans, not string values */}
+            <input 
+                type="checkbox"
+                checked={inputObject.isFriendly}
+                onChange={handleInputChange}
+                name="isFriendly"
+            />
+            {/* this is how you label the checkbox input, htmlFor is used to identify which input this label belongs to */}
+            <label htmlFor="isFriendly">Are you friendly?</label>
+
+
+            {/* radio buttons */}
+            {/* the way to make sure only one of the radio buttons can be selected at any one time, is to make them all have the same name property */}
+            <fieldset>
+                <legend>Current Employment Status</legend>
+
+
+                <input 
+                    type="radio"
+                    name="employment"
+                    id="unemployed"
+                    value="unemployed"
+                    onChange={handleInputChange}
+                    checked={inputObject.employment === 'unemployed'}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+
+
+                <input 
+                    type="radio"
+                    name="employment"
+                    id="part-time"
+                    value="part-time"
+                    onChange={handleInputChange}
+                    checked={inputObject.employment === 'part-time'}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+
+
+                <input 
+                    type="radio"
+                    name="employment"
+                    id="full-time"
+                    value="full-time"
+                    onChange={handleInputChange}
+                    checked={inputObject.employment === 'full-time'}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+            </fieldset>
         </form>
     )
 }
